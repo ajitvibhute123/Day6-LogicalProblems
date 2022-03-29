274a5cc2b0d452234317d40a4e66d4036face92a
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-03-30 00:18:25 +0530

    Add 1 file

---
+++ fibonacciSeries.java
@@ -0,0 +1,20 @@
+
+public class fibonacciSeries {
+
+	public static void main(String[] args) {
+		
+		int a = 0, b=1,c,count=10;
+		
+		for(int i=0; i<=count; i++)
+		{
+			System.out.println(+a);
+		
+		c=a+b;
+		a=b;
+		b=c;
+		
+		}
+		
+	}
+
+}
